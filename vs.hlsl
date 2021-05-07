527a4aabf13c091a383aed6d6b3719c28ace4883
Jeff Muizelaar <jrmuizel@gmail.com>
2021-05-07 10:35:03 -0400

    min

--- vs.hlsl
+++ vs.hlsl
@@ -22,10 +22,7 @@ int int_ctor(uint x0)
 {
     return int(x0);
 }
-uint uint_ctor(int x0)
-{
-    return uint(x0);
-}
+
 // Uniforms
 
 uniform float4x4 _uTransform : register(c1);
@@ -84,15 +81,13 @@ cbuffer DriverConstants : register(b1)
     SamplerMetadata samplerMetadata[5] : packoffset(c4);
 };
 
-int2 gl_texture2DSize(uint samplerIndex, int lod)
+int2 gl_texture2DSize(uint samplerIndex)
 {
     int baseLevel = samplerMetadata[samplerIndex].baseLevel;
     uint width;
     uint height;
     uint numberOfLevels;
     textures2D[samplerIndex].GetDimensions(baseLevel, width, height, numberOfLevels);
-    width = max(width >> lod, 1);
-    height = max(height >> lod, 1);
     return int2(width, height);
 }
 
@@ -100,14 +95,6 @@ float4 gl_texture2DFetch(uint samplerIndex, int2 t, int mip)
 {
     return textures2D[samplerIndex].Load(int3(t.x, t.y, mip));
 }
-
-int4 gl_texture2D_int4_Fetch(uint samplerIndex, int2 t, int mip)
-{
-    const uint textureIndex = samplerIndex - textureIndexOffset2D_int4_;
-    const uint samplerArrayIndex = samplerIndex - samplerIndexOffset2D_int4_;
-    return textures2D_int4_[textureIndex].Load(int3(t.x, t.y, mip));
-}
-
 
 struct VS_INPUT
 {
@@ -138,29 +125,20 @@ VS_OUTPUT main(VS_INPUT input)
 {
     initAttributes(input);
 
+    (gl_Position = mul(transpose(_uTransform), float4((float2(10.00, 10.00) + (float2(427.00, 256.00) * _aPosition)), 1., 1.)));
 
-    float4 _tmpvar_31039 = {10.00, 10.00, 427.00, 256.00};
-    float2 _tmpvar_71043 = (_tmpvar_31039.xy + (_tmpvar_31039.zw * _aPosition));
-
-    float4 _tmpvar_81044 = {0, 0, 0, 0};
-
-    (_tmpvar_81044.xy = (_tmpvar_71043 + ((-float4(0.00, 0.00, 0.00, 256.00).yz) + float4(0.00, 0.00, 0.00, 256.00).xy)));
-    (_tmpvar_81044.z = 1.0);
-    (_tmpvar_81044.w = 1.0);
-    (gl_Position = mul(transpose(_uTransform), _tmpvar_81044));
-
-    float2 texture_size = {0, 0};
+    float2 texture_size = {0., 0.};
 
     int format = int_ctor(gl_texture2DFetch(_sGpuCache, int2(1022, 1), 0).z);
 
-    float2 uv = (float2(427.0, 640.0) * ((_tmpvar_71043 - _tmpvar_31039.xy) / _tmpvar_31039.zw));
+    float2 uv = (float2(427.0, 640.0) * _aPosition);
 
     if (format == 1) {
-        texture_size = vec2_ctor(gl_texture2DSize(_sColor0, 0));
+        texture_size = vec2_ctor(gl_texture2DSize(_sColor0));
     } else if (format == 0) {
-        texture_size = vec2_ctor(gl_texture2DSize(_sColor0, 0));
+        texture_size = vec2_ctor(gl_texture2DSize(_sColor0));
     } if (format == 2) {
-        texture_size = vec2_ctor(gl_texture2DSize(_sColor0, 0));
+        texture_size = vec2_ctor(gl_texture2DSize(_sColor0));
     }
 
     _vUv_Y = (uv / texture_size);
